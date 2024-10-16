c7c0efad27b4cd022503e8a138436e7b6ff54adb
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2024-10-16 12:48:27 +0200

    Misc improvements
    * Handle nils in And and Or
    * Add Filter and FilterCopy funcs

--- predicate.go
+++ predicate.go
@@ -11,6 +11,9 @@ func (p P[T]) And(ps ...P[T]) P[T] {
 				return false
 			}
 		}
+		if p == nil {
+			return true
+		}
 		return p(v)
 	}
 }
@@ -23,6 +26,9 @@ func (p P[T]) Or(ps ...P[T]) P[T] {
 				return true
 			}
 		}
+		if p == nil {
+			return false
+		}
 		return p(v)
 	}
 }
@@ -32,4 +38,28 @@ func (p P[T]) Negate() P[T] {
 	return func(v T) bool {
 		return !p(v)
 	}
+}
+
+// Filter returns a new slice holding only the elements of s that satisfy p.
+// Filter modifies the contents of the slice s and returns the modified slice, which may have a smaller length.
+func (p P[T]) Filter(s []T) []T {
+	var n int
+	for _, v := range s {
+		if p(v) {
+			s[n] = v
+			n++
+		}
+	}
+	return s[:n]
+}
+
+// FilterCopy returns a new slice holding only the elements of s that satisfy p.
+func (p P[T]) FilterCopy(s []T) []T {
+	var result []T
+	for _, v := range s {
+		if p(v) {
+			result = append(result, v)
+		}
+	}
+	return result
 }
